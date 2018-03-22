427050cadcc095afe7a7f343e672ddbd527f6f59
unknown <osbornejw@CS-317-9K6NFQ1.appd.appstate.edu>
2018-03-22 14:22:36 -0400

    init commit

---
+++ ACXExample/main.c
@@ -0,0 +1,19 @@
+/*
+ * ACXExample.c
+ *
+ * Created: 3/22/2018 2:13:17 PM
+ * Author : osbornejw
+ */ 
+
+#include <avr/io.h>
+#include "acx.h"
+#include "acxserial.h"
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
+
