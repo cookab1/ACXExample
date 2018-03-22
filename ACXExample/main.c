16d2058b6fbfecd9dac020e8b35c4b2685521861
Cook <cookab1@appd.appstate.edu>
2018-03-22 14:38:58 -0400

    init commit

--- ACXExample/main.c
+++ ACXExample/main.c
@@ -9,11 +9,58 @@
 #include "acx.h"
 #include "acxserial.h"
 
+void thread1();
+void thread2();
+void thread3();
+void thread4();
+void helloThread();
+
 int main(void)
 {
+	DDRK = 0x0f; // bits 0-3 are output
+	PORTK = 0x00;  // clear to zero
+	
+	// after calling x_init(), the running thread is "thread 0"
+	x_init();
+	
+	// We are thread 0 now
+	
+	
     /* Replace with your application code */
     while (1) 
     {
+		
     }
 }
 
+void thread1() {
+	while(1)
+	{
+		
+	}
+}
+
+void thread2() {
+	while(1)
+	{
+		
+	}
+}
+void thread3() {
+	while(1)
+	{
+		
+	}
+}
+void thread4() {
+	while(1)
+	{
+		
+	}
+}
+void helloThread() {
+	while(1)
+	{
+		
+	}
+}
